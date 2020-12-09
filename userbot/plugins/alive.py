c2a576fdf789ac50914477f33e23d0aa187a3bc6
allianceprojects <71387458+allianceprojects@users.noreply.github.com>
2020-12-09 04:11:04 +0000

    Create alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -70,7 +70,7 @@ async def amireallyalive(alive):
     cat_caption += f"**  -Master :** {mention}\n"
     cat_caption += f"**  -Python Version :** `{python_version()}\n`"
     cat_caption += f"**  -Telethon version :** `{version.__version__}\n`"
-    cat_caption += f"**  -Catuserbot Version :** `{catversion}`\n"
+    cat_caption += f"**  -Okiniohelp Version :** `{catversion}`\n"
     results = await bot.inline_query(tgbotusername, cat_caption)  # pylint:disable=E0602
     await results[0].click(alive.chat_id, reply_to=reply_to_id, hide_via=True)
     await alive.delete()
