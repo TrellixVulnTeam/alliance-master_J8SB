7df72beeeef0f56f35b9161beb4f9c53c2374297
allianceprojects <71387458+allianceprojects@users.noreply.github.com>
2020-12-15 13:39:31 +0700

    Add files via upload

---
+++ userbot/plugins/adzan.py
@@ -0,0 +1,47 @@
+# ported from uniborg
+# https://github.com/muhammedfurkan/UniBorg/blob/master/stdplugins/ezanvakti.py
+import json
+
+import requests
+
+from ..utils import admin_cmd, sudo_cmd
+from . import CMD_HELP
+
+
+@bot.on(admin_cmd(pattern="adzan (.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="adzan (.*)", allow_sudo=True))
+async def get_adzan(adzan):
+    LOKASI = adzan.pattern_match.group(1)
+    url = f"https://api.pray.zone/v2/times/today.json?city={LOKASI}"
+    request = requests.get(url)
+    if request.status_code != 200:
+        await edit_delete(
+            adzan, f"`Tidak dapat mengambil data kota {LOKASI}`", 5
+        )
+        return
+    result = json.loads(request.text)
+    catresult = f"<b>Jadwal Sholat </b>\
+            \n\n<b>Kota     : </b><i>{result['results']['location']['city']}</i>\
+            \n<b>Desa  : </b><i>{result['results']['location']['country']}</i>\
+            \n<b>Tanggal     : </b><i>{result['results']['datetime'][0]['date']['gregorian']}</i>\
+            \n<b>Hijri    : </b><i>{result['results']['datetime'][0]['date']['hijri']}</i>\
+            \n\n<b>Imsak    : </b><i>{result['results']['datetime'][0]['times']['Imsak']}</i>\
+            \n<b>Sunrise  : </b><i>{result['results']['datetime'][0]['times']['Sunrise']}</i>\
+            \n<b>Fajr     : </b><i>{result['results']['datetime'][0]['times']['Fajr']}</i>\
+            \n<b>Dhuhr    : </b><i>{result['results']['datetime'][0]['times']['Dhuhr']}</i>\
+            \n<b>Asr      : </b><i>{result['results']['datetime'][0]['times']['Asr']}</i>\
+            \n<b>Sunset   : </b><i>{result['results']['datetime'][0]['times']['Sunset']}</i>\
+            \n<b>Maghrib  : </b><i>{result['results']['datetime'][0]['times']['Maghrib']}</i>\
+            \n<b>Isha     : </b><i>{result['results']['datetime'][0]['times']['Isha']}</i>\
+            \n<b>Tengah malam : </b><i>{result['results']['datetime'][0]['times']['Midnight']}</i>\
+    "
+    await edit_or_reply(adzan, catresult, "html")
+
+
+CMD_HELP.update(
+    {
+        "adzan": "__**PLUGIN NAME :** Adzan__\
+    \n\n📌** CMD ➥** `.adzan` <city name>\
+    \n**USAGE   ➥  **__Shows you the Islamic prayer times of the given city name__"
+    }
+)
---
+++ userbot/plugins/coinexc.py
@@ -0,0 +1,56 @@
+# created by @okinio
+
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from ..utils import admin_cmd, edit_or_reply, sudo_cmd
+from . import CMD_HELP
+
+
+@bot.on(admin_cmd(outgoing=True, pattern="xc_help$"))
+@bot.on(sudo_cmd(outgoing=True, pattern="xc_help$", allow_sudo=True))
+async def dogeads(myxc):
+    await edit_or_reply(
+        myxc, "Semua perintah ada [disini](https://nekobin.com/pemutuzeri) "
+    )
+
+
+@bot.on(admin_cmd(pattern="xc(?: |$)(.*)", outgoing=True))
+@bot.on(sudo_cmd(outgoing=True, pattern="xc(?: |$)(.*)", allow_sudo=True))
+async def dogeads(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    if link in "":
+        link = ""
+    elif link in "":
+        link = ""
+    elif link == "":
+        link = ""
+    catevent = await edit_or_reply(event, "```Sedang mengirim hasil....```")
+    async with event.client.conversation("@CryptoConvBot") as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=178947289)
+            )
+            await conv.send_message(f"{link}")
+            response = await response
+        except YouBlockedUserError:
+            await catevent.edit("```Unblock @CryptoConvBot nya cok```")
+            return
+        else:
+            await catevent.delete()
+            await event.client.forward_messages(event.chat_id, response.message)
+            await event.client.send_read_acknowledge(conv.chat_id)
+
+
+CMD_HELP.update(
+    {
+        "adsbot": "__**PLUGIN NAME :** Adsbot__\
+    \n\n📌** CMD ➥** `.xc_help` \
+    \n**USAGE   ➥  **untuk mendapatkan daftar perintah.\
+    \n\n📌** CMD ➥** `.xc <kode perintah>`\
+    \n**USAGE   ➥  **Ketik .xc <coin> to <coin> untuk mendapatkan respon.\
+    \n\n**Contoh:** `.xc 1 btc idr` , ini perintah untuk memeriksa balance."
+    }
+)
