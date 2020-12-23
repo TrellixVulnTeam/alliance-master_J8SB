5e06e7c857b5ee5dce6587410dceef10d2263bf8
allianceprojects <71387458+allianceprojects@users.noreply.github.com>
2020-12-23 12:20:17 +0700

    Add files via upload

--- userbot/plugins/transferuang.py
+++ userbot/plugins/transferuang.py
@@ -9,27 +9,27 @@ from . import ALIVE_NAME, CMD_HELP, mention
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
 
 
-@bot.on(admin_cmd(pattern=r"tfx (.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern=r"tfx (.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"tf (.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern=r"tf (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
     name = event.pattern_match.group(1)
     animation_interval = 5
     animation_ttl = range(11)
-    event = await edit_or_reply(event, "Mentransfer uang...")
+    event = await edit_or_reply(event, "{DEFAULTUSER} Melakukan Transfer Uang...")
     animation_chars = [
                 "`Menyambungkan Ke Server Rekening...`",
-                "`✅ Login Ke Rekening {DEFAULTUSER} Sukses.`",
-                "`Proses Transfer Uang Ke {name}... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-                "`Proses Transfer Uang Ke {name}... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-                "`Proses Transfer Uang Ke {name}... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-                "`Proses Transfer Uang Ke {name}... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-                "`Proses Transfer Uang Ke {name}... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-                "`Proses Transfer Uang Ke {name}... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
-                "`Proses Transfer Uang Ke {name}... 84%\n█████████████████████▒▒▒▒ `",
-                "`Proses Transfer Uang Ke {name}... 100%\n██████████████████████████ `",
-                f"`Proses Transfer Selesai...\n\n{mention} Telah Mengirim Uang Sebesar 110$ Ke` {name} \n\n`✅ Transaksi Sukses..`",
+                "`✅ Login Ke Rekening` {DEFAULTUSER} `Sukses.`",
+                "`Proses Transfer Uang Ke` {name} | `0%`\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ",
+                "`Proses Transfer Uang Ke` {name} | `4%`\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ",
+                "`Proses Transfer Uang Ke` {name} | `8%`\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ",
+                "`Proses Transfer Uang Ke` {name} | `20%`\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ",
+                "`Proses Transfer Uang Ke` {name} | `36%`\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ",
+                "`Proses Transfer Uang Ke` {name} | `52%`\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ ",
+                "`Proses Transfer Uang Ke` {name} | `84%`\n█████████████████████▒▒▒▒ ",
+                "`Proses Transfer Uang Ke` {name} | `100%`\n██████████████████████████ ",
+                f"`Proses Transfer Selesai...`\n\n{mention} `Telah Mengirim Uang Sebesar 110$ Ke` {name} \n\n`✅ Transaksi Sukses..`",
             ]
     for i in animation_ttl:
         await asyncio.sleep(animation_interval)
@@ -37,8 +37,8 @@ async def _(event):
 
 CMD_HELP.update(
     {
-        "tfx": "__**PLUGIN NAME :** tfx__\
-    \n\n📌** CMD ➥** `.tfx` reply to a person\
+        "tf": "__**PLUGIN NAME :** tf__\
+    \n\n📌** CMD ➥** `.tf` reply to a person\
 "
     }
 )
